b8f5fc8579a9d4904556c7fe202f7afc48945dc0
mohammad-dehghanifard <m.deheghanifard@gmail.com>
2023-06-21 10:25:43 -0700

    refactor code and impl isWhite Helper Function

--- lib/view/game_board.dart
+++ lib/view/game_board.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chess_game/component/helper_function.dart';
 import 'package:flutter_chess_game/widget/square.dart';
 
 class GameBoardView extends StatefulWidget {
@@ -16,16 +17,7 @@ class _GameBoardViewState extends State<GameBoardView> {
         itemCount: 8 * 8,  // تعداد کل عناصر در GridView (64 عنصر)
         physics: const NeverScrollableScrollPhysics(),  // غیرفعال کردن امکان اسکرول کردن GridView
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 8),  // تعیین تنظیمات جدول (8 ستون)
-        itemBuilder: (context, index) {
-
-          int y = index % 8;  // محاسبه شماره ستون براساس شماره عنصر
-          int x = index ~/ 8;  // محاسبه شماره ردیف براساس شماره عنصر
-          // عملگر ~/ برای تقسیم دو عدد به صورتی استفاده می‌شود که فقط بخش صحیح نتیجه تقسیم باقی می‌ماند و اعشار حذف می‌شود.
-
-          bool isWhite = (x + y) % 2 == 0;  // تشخیص رنگ مربع بر اساس موقعیت ستون و ردیف
-
-          return Square(isWhite: isWhite);  // ایجاد عنصر مربعی با رنگ مشخص شده
-        },
+        itemBuilder: (context, index) => Square(isWhite: isWhite(index)) // ایجاد عنصر مربعی با رنگ مشخص شده
       ),
 
     );
