0a2329317a7f656ca333ee04e0da33905f9242c5
mohammad-dehghanifard <m.deheghanifard@gmail.com>
2023-06-22 00:05:17 -0700

    update board game and set piece

--- lib/view/game_board.dart
+++ lib/view/game_board.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_chess_game/component/helper_function.dart';
+import 'package:flutter_chess_game/model/chess_piece.dart';
 import 'package:flutter_chess_game/widget/square.dart';
+
+import '../gen/assets.gen.dart';
 
 class GameBoardView extends StatefulWidget {
   const GameBoardView({Key? key}) : super(key: key);
@@ -12,12 +15,16 @@ class GameBoardView extends StatefulWidget {
 class _GameBoardViewState extends State<GameBoardView> {
   @override
   Widget build(BuildContext context) {
+    ChessPiece myPiece = ChessPiece(type: ChessPieceType.pawn, iconPath: Assets.icons.chessPawn.path, isWhite: false);
     return Scaffold(
-      body: GridView.builder(
-        itemCount: 8 * 8,  // تعداد کل عناصر در GridView (64 عنصر)
-        physics: const NeverScrollableScrollPhysics(),  // غیرفعال کردن امکان اسکرول کردن GridView
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 8),  // تعیین تنظیمات جدول (8 ستون)
-        itemBuilder: (context, index) => Square(isWhite: isWhite(index)) // ایجاد عنصر مربعی با رنگ مشخص شده
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.builder(
+          itemCount: 8 * 8,  // تعداد کل عناصر در GridView (64 عنصر)
+          physics: const NeverScrollableScrollPhysics(),  // غیرفعال کردن امکان اسکرول کردن GridView
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 8),  // تعیین تنظیمات جدول (8 ستون)
+          itemBuilder: (context, index) => Square(isWhite: isWhite(index),piece: myPiece) // ایجاد عنصر مربعی با رنگ مشخص شده
+        ),
       ),
 
     );
--- lib/widget/square.dart
+++ lib/widget/square.dart
@@ -1,12 +1,17 @@
+
 import 'package:flutter/material.dart';
+import 'package:flutter_chess_game/model/chess_piece.dart';
 
 class Square extends StatelessWidget {
-  const Square({Key? key, required this.isWhite}) : super(key: key);
+  const Square({Key? key, required this.isWhite, this.piece}) : super(key: key);
   final bool isWhite;
+  final ChessPiece? piece;
   @override
   Widget build(BuildContext context) {
     return Container(
-    color: isWhite ? Colors.grey[200] : Colors.grey[500],
+    color: isWhite ? Colors.grey[400] : Colors.grey[600],
+     padding: const EdgeInsets.all(8),
+     child: piece != null ? Image.asset(piece!.iconPath,color: piece!.isWhite? Colors.white : Colors.black) : null,
     );
   }
 }
