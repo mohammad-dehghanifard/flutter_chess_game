966fa34b206bf483be13fedb835ecf9a0b251730
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2023-07-01 23:39:15 -0700

    refactor finishGameDialog

--- lib/component/finish_game_dialog.dart
+++ lib/component/finish_game_dialog.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 Future<dynamic> finishGameDialog({required BuildContext context,required Function() onTap}) {
   return showDialog(context: context, builder: (context) => AlertDialog(
-    title: const Text('بازی تموم شد رفیق...'),
-    backgroundColor: Colors.grey[300],
+    title:  Text('Restart Game?',style: GoogleFonts.adamina(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.brown[100]),),
+    backgroundColor: Colors.brown[400],
     actions: [
       TextButton(
           onPressed: onTap,
-          child: const Text("شروع مجدد بازی")),
+          child: Text('Yes',style: GoogleFonts.adamina(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.brown[100]),)),
       TextButton(
           onPressed: () {},
-          child: const Text("برگشت به صفحه اصلی")),
+          child: Text('No',style: GoogleFonts.adamina(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.brown[100]),)),
     ],
   ),);
 }
